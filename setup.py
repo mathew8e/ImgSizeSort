edcebef7ab5abedf33d0e26dda0c7cd9d81fcd95
mathew8e <123kumatan@gmail.com>
2022-07-04 10:55:41 +0200

    module not working

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ImgSizeSort',
+      version='0.1',
+      description='A plugin that sorts out images by the ration of its size',
+      author='Orson',
+      url='https://github.com/mathew8e/ImgSizeSort',
+      author_email='123kumatan@gmail.com',
+      license='MIT',
+      packages=['ImgSizeSort'],
+      zip_safe=False)
