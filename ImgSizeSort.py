f676288a78ee9b8af034a4726cd810081511223b
mathew8e <123kumatan@gmail.com>
2022-07-04 08:46:33 +0200

    somewhat working

--- ImgSizeSort.py
+++ ImgSizeSort.py
@@ -32,6 +32,7 @@ class ImgSizeSort():
                 img = Image.open("{0}/{1}".format(PATH,i))
                 w, h  = img.size
                 ArrayWithImgObjects.append(ImageObj(i, w, h))
+                img.close()
             
 
         for image in ArrayWithImgObjects:
@@ -39,8 +40,11 @@ class ImgSizeSort():
             print("Ratio: {0}     Width: {1}    Height: {2}    Name: {3}".format(image.ratio, image.width, image.height, image.name))
             if image.ratio < 0.9:
                 shutil.move("{0}/{1}".format(PATH, image.name), "{0}/{1}".format(PATH, "portrait"))
-            
-
+            elif image.ratio >= 0.9 and image.ratio <= 1.1:
+                shutil.move("{0}/{1}".format(PATH, image.name), "{0}/{1}".format(PATH, "square"))
+            else:
+                shutil.move("{0}/{1}".format(PATH, image.name), "{0}/{1}".format(PATH, "landscape"))
+                
 
 PATH = path.abspath(r"C:\Users\mathe\Downloads\OrientalitstPhotos")
 ImgSizeSort.sort(PATH=PATH)
