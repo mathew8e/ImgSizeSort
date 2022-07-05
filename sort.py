a4f4038807c8ecb5a6af6d14f804c90155e872d0
mathew8e <123kumatan@gmail.com>
2022-07-05 19:42:26 +0200

    Added a sort wide to tall function some bugs must be fixed

--- sort.py
+++ sort.py
@@ -1,15 +1,34 @@
 from email.header import Header
+from multiprocessing.dummy import Array
+import os
 from pathlib import Path
+import string
 from turtle import width
 from PIL import Image
 from os import path, mkdir, walk, listdir
 import shutil
 
 
+def makeImgObjArray(PATH):
+    ArrayWithImgObjects = []
+    class ImageObj():
+        def __init__(self, name, width, height):
+            self.name = name
+            self.width = width
+            self.height = height
+            self.ratio = width/height
+                
+    for i in listdir(PATH):
+        if i.endswith(".png") or i.endswith(".jpg"):
+            img = Image.open("{0}/{1}".format(PATH,i))
+            w, h  = img.size
+            ArrayWithImgObjects.append(ImageObj(i, w, h))
+            img.close()
+    
+    return ArrayWithImgObjects
 
 def sort(PATH):
     try:
-        ArrayWithImgObjects = []
 
         def createFolders():
             mkdir("{0}\\portrait".format(PATH))
@@ -21,19 +40,7 @@ def sort(PATH):
         else:
             return 2
             
-        class ImageObj():
-            def __init__(self, name, width, height):
-                self.name = name
-                self.width = width
-                self.height = height
-                self.ratio = width/height
-                
-        for i in listdir(PATH):
-            if i.endswith(".png") or i.endswith(".jpg"):
-                img = Image.open("{0}/{1}".format(PATH,i))
-                w, h  = img.size
-                ArrayWithImgObjects.append(ImageObj(i, w, h))
-                img.close()
+        ArrayWithImgObjects = makeImgObjArray(PATH)
             
 
         for image in ArrayWithImgObjects:
@@ -48,4 +55,22 @@ def sort(PATH):
         return 1
     except:
         return 0
-            
+    
+def sortWideToTall(PATH):
+    # sort the images by ratio and puts an ascii characet infromt
+    # try:
+        ArrayWithImgObjects = makeImgObjArray(PATH)
+        
+        ArrayWithImgObjects.sort(key=lambda x: x.ratio)
+        asciiList = []
+        
+        for letter1 in list(string.ascii_letters):
+            for letter2 in list(string.ascii_letters):
+                for letter3 in list(string.ascii_letters):
+                    asciiList.append("{0}{1}{2}".format(letter1, letter2, letter3))
+        for i in range(len(ArrayWithImgObjects)):
+            os.rename("{0}/{1}".format(PATH, ArrayWithImgObjects[i].name), "{0}/{1}_{2}".format(PATH, asciiList[i], ArrayWithImgObjects[i].name))
+            print(ArrayWithImgObjects[i].ratio)
+        return 1
+    # except:
+    #     return 0
